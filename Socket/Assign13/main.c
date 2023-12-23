e06ee42eb6980e80b47f9049a330b1059634682c
dat0eight <datnguyenqt@gmail.com>
2023-12-23 21:50:20 +0700

    Add Socket Assignments and Pipe-FIFOs Assignment

---
+++ Socket/Assign13/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <sys/socket.h>
+
+
+void chatBox()
+{
+
+}
+
+void main (char *argv[] , int argc)
+{
+    
+    socket()
+
+    bind()
+
+    listen()
+
+    accept()
+
+    
+    close ()
+}
